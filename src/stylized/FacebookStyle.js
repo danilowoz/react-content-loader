d3f4266bf6afeea5f357982b4d4b6fca50869cf0
Ricardo M. Lima <ricardolima89@gmail.com>
2017-12-21 18:28:15 -0200

    Updated facebook style - close #42

--- src/stylized/FacebookStyle.js
+++ src/stylized/FacebookStyle.js
@@ -5,14 +5,12 @@ import type { WrapProps } from '../Wrap'
 
 const FacebookStyle = (props: WrapProps): React.Element<*> => (
   <Wrap {...props}>
-    <rect x="0" y="0" rx="5" ry="5" width="70" height="70" />
-
-    <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
-    <rect x="80" y="40" rx="3" ry="3" width="250" height="10" />
-
-    <rect x="0" y="80" rx="3" ry="3" width="350" height="10" />
-    <rect x="0" y="100" rx="3" ry="3" width="400" height="10" />
-    <rect x="0" y="120" rx="3" ry="3" width="360" height="10" />
+	<rect x="70" y="15.82" rx="4" ry="4" width="117" height="6.4" />
+	<rect x="70" y="36.05" rx="3" ry="3" width="85" height="6.4" />
+	<rect x="2" y="81" rx="3" ry="3" width="350" height="6.4" />
+	<rect x="0" y="102.38" rx="3" ry="3" width="380" height="6.4" />
+	<rect x="0" y="123.26" rx="3" ry="3" width="201.6" height="6.4" />
+	<circle cx="30.284271247461902" cy="29.334271247461903" r="28.284271247461902" />
   </Wrap>
 )
 
