6008e29bb51bb5df5a64ad0193a9150a0e130659
lasfin <evgenij-kopica@yandex.ru>
2017-12-28 18:56:37 +0300

    fix indentation, move 'when type is custom' test from 'Type props are used' block

--- tests/index.js
+++ tests/index.js
@@ -53,18 +53,18 @@ describe('<ContentLoader />:', () => {
 
   describe('when type is custom', () => {
 
-      const wrapper = mount(
-          <ContentLoader>
-            <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
-            <rect x="82" y="44" rx="3" ry="3" width="250" height="10" />
-            <circle cx="35" cy="35" r="35" />
-          </ContentLoader>
-      )
+    const wrapper = mount(
+      <ContentLoader>
+        <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
+        <rect x="82" y="44" rx="3" ry="3" width="250" height="10" />
+        <circle cx="35" cy="35" r="35" />
+      </ContentLoader>
+    )
 
-      it('should render custom element', () => {
-          expect(wrapper.find('rect')).to.have.length(3)
-          expect(wrapper.find('circle')).to.have.length(1)
-      })
+    it('should render custom element', () => {
+      expect(wrapper.find('rect')).to.have.length(3)
+      expect(wrapper.find('circle')).to.have.length(1)
+    })
   })
 
   describe('Props are propagated', () => {
