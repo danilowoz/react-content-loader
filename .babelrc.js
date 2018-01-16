d89fdcf3332b3fd2d6a907a1d26f29488235d521
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-01-16 21:27:00 +0100

    Refactored build infrastructure (#68)

---
+++ .babelrc.js
@@ -0,0 +1,12 @@
+const { NODE_ENV, BABEL_ENV } = process.env
+const modules = BABEL_ENV === 'cjs' || NODE_ENV === 'test' ? 'commonjs' : false
+const loose = true
+
+module.exports = {
+  presets: [
+    ["env", { modules, loose, targets: { uglify: true } }],
+    'stage-2',
+    'react',
+    'flow',
+  ]
+}
---
+++ src/uid.js
@@ -0,0 +1,2 @@
+// @flow
+export default (): string => Math.random().toString(36).substring(2)
