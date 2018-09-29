15f8d66703e93a2dc44d97f22902a27ce520c76e
Danilo Woznica <danilowoz@gmail.com>
2018-09-29 13:20:52 +0100

    Improv code

--- src/Wrap.js
+++ src/Wrap.js
@@ -1,11 +1,11 @@
-//@flow
-import * as React from 'react'
+// @flow
+import * as React from "react"
 
-import uid from './uid'
-import type { Props as ContentLoaderProps } from './index'
+import uid from "./uid"
+import type { Props as ContentLoaderProps } from "./index"
 
 export type WrapProps = {
-  children?: React.ChildrenArray<*>,
+  children?: React.ChildrenArray<*>
 } & ContentLoaderProps
 
 const Wrap = (props: WrapProps): React.Element<*> => {
@@ -15,7 +15,6 @@ const Wrap = (props: WrapProps): React.Element<*> => {
   return (
     <svg
       viewBox={`0 0 ${props.width} ${props.height}`}
-      version="1.1"
       style={props.style}
       preserveAspectRatio={props.preserveAspectRatio}
       className={props.className}
@@ -33,35 +32,49 @@ const Wrap = (props: WrapProps): React.Element<*> => {
         <clipPath id={idClip}>{props.children}</clipPath>
 
         <linearGradient id={idGradient}>
-          <stop offset="0%" stopColor={props.primaryColor} stopOpacity={props.primaryOpacity}>
-            {props.animate ? (
+          <stop
+            offset="0%"
+            stopColor={props.primaryColor}
+            stopOpacity={props.primaryOpacity}
+          >
+            {props.animate && (
               <animate
                 attributeName="offset"
                 values="-2; 1"
                 dur={`${props.speed}s`}
                 repeatCount="indefinite"
               />
-            ) : null}
+            )}
           </stop>
-          <stop offset="50%" stopColor={props.secondaryColor} stopOpacity={props.secondaryOpacity}>
-            {props.animate ? (
+
+          <stop
+            offset="50%"
+            stopColor={props.secondaryColor}
+            stopOpacity={props.secondaryOpacity}
+          >
+            {props.animate && (
               <animate
                 attributeName="offset"
-                values="-1.5; 1.5"
+                values="-1.5; 2.5"
                 dur={`${props.speed}s`}
                 repeatCount="indefinite"
               />
-            ) : null}
+            )}
           </stop>
-          <stop offset="100%" stopColor={props.primaryColor} stopOpacity={props.primaryOpacity}>
-            {props.animate ? (
+
+          <stop
+            offset="100%"
+            stopColor={props.primaryColor}
+            stopOpacity={props.primaryOpacity}
+          >
+            {props.animate && (
               <animate
                 attributeName="offset"
-                values="-1; 2"
+                values="-1; 3"
                 dur={`${props.speed}s`}
                 repeatCount="indefinite"
               />
-            ) : null}
+            )}
           </stop>
         </linearGradient>
       </defs>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-//@flow
-import * as React from 'react'
-import Wrap from './Wrap'
+// @flow
+import * as React from "react"
+import Wrap from "./Wrap"
 
 // Stylized
-export { default as Facebook } from './stylized/FacebookStyle'
-export { default as Instagram } from './stylized/InstagramStyle'
-export { default as Code } from './stylized/CodeStyle'
-export { default as List } from './stylized/ListStyle'
-export { default as BulletList } from './stylized/BulletListStyle'
+export { default as Facebook } from "./stylized/FacebookStyle"
+export { default as Instagram } from "./stylized/InstagramStyle"
+export { default as Code } from "./stylized/CodeStyle"
+export { default as List } from "./stylized/ListStyle"
+export { default as BulletList } from "./stylized/BulletListStyle"
 
 export type Props = {
   animate: boolean,
@@ -21,7 +21,7 @@ export type Props = {
   primaryOpacity: number,
   secondaryOpacity: number,
   style: { [key: string]: any },
-  uniquekey: string,
+  uniquekey: string
 }
 
 export const defaultProps = {
@@ -29,11 +29,11 @@ export const defaultProps = {
   speed: 2,
   width: 400,
   height: 130,
-  preserveAspectRatio: 'xMidYMid meet',
-  primaryColor: '#f0f0f0',
-  secondaryColor: '#e0e0e0',
+  preserveAspectRatio: "xMidYMid meet",
+  primaryColor: "#f0f0f0",
+  secondaryColor: "#e0e0e0",
   primaryOpacity: 1,
-  secondaryOpacity: 1,
+  secondaryOpacity: 1
 }
 
 const InitialComponent = props => (
@@ -42,7 +42,11 @@ const InitialComponent = props => (
 
 const ContentLoader = (props: Props) => {
   const mergedProps = { ...defaultProps, ...props }
-  const children = props.children ? props.children : <InitialComponent {...mergedProps} />
+  const children = props.children ? (
+    props.children
+  ) : (
+    <InitialComponent {...mergedProps} />
+  )
 
   return <Wrap {...mergedProps}>{children}</Wrap>
 }
