f596d4c0c98e8aa24c96fd8c1211eb2cc667095b
Danilo Woznica <danilowoz@gmail.com>
2018-09-29 13:43:07 +0100

    test(Tests): Code style

--- tests/stylized/CodeStyle.js
+++ tests/stylized/CodeStyle.js
@@ -1,25 +1,25 @@
-import 'jsdom-global/register'
-import React from 'react'
+import "jsdom-global/register"
+import React from "react"
 
-import Enzyme, { mount } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import Enzyme, { mount } from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
 
-import chai, { expect } from 'chai'
-import chaiEnzyme from 'chai-enzyme'
+import chai, { expect } from "chai"
+import chaiEnzyme from "chai-enzyme"
 
 Enzyme.configure({ adapter: new Adapter() })
 chai.use(chaiEnzyme())
 
-import CodeStyle from '../../src/stylized/CodeStyle'
+import CodeStyle from "../../src/stylized/CodeStyle"
 
-describe('<CodeStyle />:', () => {
+describe("<CodeStyle />:", () => {
   const wrapper = mount(<CodeStyle speed={20} />)
 
-  it('has nine `rect`', () => {
-    expect(wrapper.find('clipPath rect')).to.have.length(9)
+  it("has nine `rect`", () => {
+    expect(wrapper.find("clipPath rect")).to.have.length(9)
   })
 
-  it('prop is propagated', () => {
+  it("prop is propagated", () => {
     expect(wrapper.props().speed).to.equal(20)
   })
 })
--- tests/stylized/FacebookStyle.js
+++ tests/stylized/FacebookStyle.js
@@ -1,29 +1,29 @@
-import 'jsdom-global/register'
-import React from 'react'
+import "jsdom-global/register"
+import React from "react"
 
-import Enzyme, { mount } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import Enzyme, { mount } from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
 
-import chai, { expect } from 'chai'
-import chaiEnzyme from 'chai-enzyme'
+import chai, { expect } from "chai"
+import chaiEnzyme from "chai-enzyme"
 
 Enzyme.configure({ adapter: new Adapter() })
 chai.use(chaiEnzyme())
 
-import FacebookStyle from '../../src/stylized/FacebookStyle'
+import FacebookStyle from "../../src/stylized/FacebookStyle"
 
-describe('<FacebookStyle />', () => {
+describe("<FacebookStyle />", () => {
   const wrapper = mount(<FacebookStyle speed={20} />)
 
-  it('has five `rect`', () => {
-    expect(wrapper.find('clipPath rect')).to.have.length(5)
+  it("has five `rect`", () => {
+    expect(wrapper.find("clipPath rect")).to.have.length(5)
   })
 
-  it('has one `circle`', () => {
-    expect(wrapper.find('clipPath circle')).to.have.length(1)
+  it("has one `circle`", () => {
+    expect(wrapper.find("clipPath circle")).to.have.length(1)
   })
 
-  it('prop is propagated', () => {
+  it("prop is propagated", () => {
     expect(wrapper.props().speed).to.equal(20)
   })
 })
