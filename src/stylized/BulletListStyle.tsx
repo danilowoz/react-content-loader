4f4bbe24a1df3ba94622a44bb3f881bf71b2a645
Ajay Poshak <poshakajay@gmail.com>
2018-12-02 17:05:38 +0530

    Convert BulletListStyle to tsx

--- src/stylized/BulletListStyle.tsx
+++ src/stylized/BulletListStyle.tsx
@@ -1,9 +1,8 @@
-// @flow
-import * as React from "react"
-import Holder from "../Holder"
-import type { SvgProps } from "../Svg"
+import * as React from "react";
+import Holder from "../Holder";
+import { ISvgProps } from "../Svg";
 
-const BulletListStyle = (props: SvgProps): React.Element<*> => (
+const BulletListStyle = (props: ISvgProps) => (
   <Holder {...props}>
     <circle cx="10" cy="20" r="8" />
     <rect x="25" y="15" rx="5" ry="5" width="220" height="10" />
@@ -14,6 +13,6 @@ const BulletListStyle = (props: SvgProps): React.Element<*> => (
     <circle cx="10" cy="110" r="8" />
     <rect x="25" y="105" rx="5" ry="5" width="220" height="10" />
   </Holder>
-)
+);
 
-export default BulletListStyle
+export default BulletListStyle;
