b80eb7d911b37b9080edf9adfb871b967a5d6372
Ajay Poshak <poshakajay@gmail.com>
2018-12-02 17:05:00 +0530

    Convert index.js to ts

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import Holder from "./Holder"
-
-export { default as Facebook } from "./stylized/FacebookStyle"
-export { default as Instagram } from "./stylized/InstagramStyle"
-export { default as Code } from "./stylized/CodeStyle"
-export { default as List } from "./stylized/ListStyle"
-export { default as BulletList } from "./stylized/BulletListStyle"
-
-export default Holder
