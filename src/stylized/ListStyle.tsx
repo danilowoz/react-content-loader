3c92d5d98e8f5a55f8dd9740db09614b4004f1bb
Ajay Poshak <poshakajay@gmail.com>
2018-12-02 17:06:54 +0530

    Convert ListStyle to tsx

--- src/stylized/ListStyle.tsx
+++ src/stylized/ListStyle.tsx
@@ -1,9 +1,8 @@
-// @flow
-import * as React from "react"
-import Holder from "../Holder"
-import type { SvgProps } from "../Svg"
+import * as React from "react";
+import Holder from "../Holder";
+import { ISvgProps } from "../Svg";
 
-const ListStyle = (props: SvgProps): React.Element<*> => (
+const ListStyle = (props: ISvgProps) => (
   <Holder {...props}>
     <rect x="0" y="0" rx="3" ry="3" width="250" height="10" />
     <rect x="20" y="20" rx="3" ry="3" width="220" height="10" />
@@ -12,6 +11,6 @@ const ListStyle = (props: SvgProps): React.Element<*> => (
     <rect x="20" y="80" rx="3" ry="3" width="200" height="10" />
     <rect x="20" y="100" rx="3" ry="3" width="80" height="10" />
   </Holder>
-)
+);
 
-export default ListStyle
+export default ListStyle;
