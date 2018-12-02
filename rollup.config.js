9e42dfd5dc75d8b6be5ed4ec0b4c2e0d1588e04a
Ajay Poshak <poshakajay@gmail.com>
2018-12-02 17:03:48 +0530

    Replace babel with ts compiler

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,20 @@
-import babel from 'rollup-plugin-babel'
-import replace from 'rollup-plugin-replace'
-import { uglify } from "rollup-plugin-uglify";
-import pkg from './package.json'
+import replace from "rollup-plugin-replace"
+import { uglify } from "rollup-plugin-uglify"
+import typescript from "rollup-plugin-typescript2"
+import pkg from "./package.json"
 
-const mergeAll = (objs) => Object.assign({}, ...objs)
+const mergeAll = objs => Object.assign({}, ...objs)
 
 const commonPlugins = [
-  babel({
-    exclude: 'node_modules/**'
-  }),
+  typescript({
+    typescript: require("typescript")
+  })
 ]
 
 const configBase = {
-  input: 'src/index.js',
+  input: "src/index.ts",
   output: {
-    exports: 'named'
+    exports: "named"
   },
   external: [
     ...Object.keys(pkg.dependencies || {}),
@@ -30,13 +30,12 @@ const umdConfig = mergeAll([
       configBase.output,
       {
         file: `dist/${pkg.name}.js`,
-        format: 'umd',
-        name: 'ContentLoader',
+        format: "umd",
+        name: "ContentLoader",
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        },
-
+          react: "React",
+          "react-dom": "ReactDOM"
+        }
       }
     ]),
     external: Object.keys(pkg.peerDependencies || {})
@@ -48,7 +47,7 @@ const devUmdConfig = mergeAll([
   {
     plugins: umdConfig.plugins.concat(
       replace({
-        'process.env.NODE_ENV': JSON.stringify('development')
+        "process.env.NODE_ENV": JSON.stringify("development")
       })
     )
   }
@@ -59,19 +58,19 @@ const prodUmdConfig = mergeAll([
   {
     output: mergeAll([
       umdConfig.output,
-      { file: umdConfig.output.file.replace(/\.js$/, '.min.js') }
+      { file: umdConfig.output.file.replace(/\.js$/, ".min.js") }
     ])
   },
   {
     plugins: umdConfig.plugins.concat(
       replace({
-        'process.env.NODE_ENV': JSON.stringify('production')
+        "process.env.NODE_ENV": JSON.stringify("production")
       }),
       uglify({
         compress: {
           pure_getters: true,
           unsafe: true,
-          unsafe_comps: true,
+          unsafe_comps: true
         }
       })
     )
@@ -82,8 +81,8 @@ const webConfig = mergeAll([
   configBase,
   {
     output: [
-      mergeAll([configBase.output, { file: pkg.module, format: 'es' }]),
-      mergeAll([configBase.output, { file: pkg.main, format: 'cjs' }])
+      mergeAll([configBase.output, { file: pkg.module, format: "es" }]),
+      mergeAll([configBase.output, { file: pkg.main, format: "cjs" }])
     ]
   }
 ])
