773ff702e6a19d3c9ae7b0d2b00c306424612730
Danilo Woznica <danilowoz@gmail.com>
2018-12-03 21:36:43 +0000

    Removed old tests

--- setupTests.js
+++
@@ -1,7 +0,0 @@
-/* eslint-env jest */
-/* eslint-disable import/no-extraneous-dependencies */
-
-import { configure } from "enzyme"
-import Adapter from "enzyme-adapter-react-16"
-
-configure({ adapter: new Adapter() })
