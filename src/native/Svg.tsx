9b96f7def4f8a63320eb40204e2a0a9af0a92a19
DrStoop <berr@gmx.de>
2025-07-07 14:32:28 +0300

    Revert "Enabling `backgroundOpacity` and `foregroundOpacity` for `ContentLoad…"
    This reverts commit 418fae973e7b54cd43ec6d6eaf50cad505108378.

--- src/native/Svg.tsx
+++ src/native/Svg.tsx
@@ -17,9 +17,7 @@ class NativeSvg extends Component<IContentLoaderProps> {
   static defaultProps = {
     animate: true,
     backgroundColor: '#f5f6f7',
-    backgroundOpacity: 1,
     foregroundColor: '#eee',
-    foregroundOpacity: 1,
     rtl: false,
     speed: 1.2,
     interval: 0.25,
@@ -76,9 +74,7 @@ class NativeSvg extends Component<IContentLoaderProps> {
     const {
       children,
       backgroundColor,
-      backgroundOpacity,
       foregroundColor,
-      foregroundOpacity,
       rtl,
       style,
       beforeMask,
@@ -128,9 +124,9 @@ class NativeSvg extends Component<IContentLoaderProps> {
             y1={0}
             y2={0}
           >
-            <Stop offset={0} stopColor={backgroundColor} stopOpacity={backgroundOpacity} />
-            <Stop offset={0.5} stopColor={foregroundColor} stopOpacity={foregroundOpacity} />
-            <Stop offset={1} stopColor={backgroundColor} stopOpacity={backgroundOpacity} />
+            <Stop offset={0} stopColor={backgroundColor} />
+            <Stop offset={0.5} stopColor={foregroundColor} />
+            <Stop offset={1} stopColor={backgroundColor} />
           </AnimatedLinearGradient>
         </Defs>
       </Svg>
